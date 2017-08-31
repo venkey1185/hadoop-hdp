980c594ec81295144035f4e025ca631de845cadf
Randy Gelhausen <rgelhau@gmail.com>
2017-08-31 15:53:59 -0400

    fixed Ambari db schema version

--- containers/postgres/scripts/100-ambari.sh
+++ containers/postgres/scripts/100-ambari.sh
@@ -3,7 +3,7 @@ set -e
 
 cd ~/
 wget https://raw.githubusercontent.com/apache/ambari/branch-2.5/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
-sed -i "s/\${ambariSchemaVersion}/2.5.1/g" Ambari-DDL-Postgres-CREATE.sql
+sed -i "s/\${ambariSchemaVersion}/2.5.2/g" Ambari-DDL-Postgres-CREATE.sql
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     create database ambari;
